71ea99046c692d6d031330f226438568f9aeaa5b
savagebananas <bruno7405@gmail.com>
2022-12-30 23:07:53 -0800

    pt 2

--- Staying Warm/Assets/Environment/LevelGeneration.cs
+++ Staying Warm/Assets/Environment/LevelGeneration.cs
@@ -8,9 +8,17 @@ public class LevelGeneration : MonoBehaviour
 
     public GameObject tree;
     public GameObject campfire;
+    public GameObject bush;
+    public GameObject bigRock;
+    public GameObject snowman;
+    public GameObject christmasTree;
 
     public float numberOfTrees;
     public float numberOfcampfires;
+    public float numberOfBush;
+    public float numberOfBigRock;
+    public float numberOfSnowman;
+    public float numberOfChristmasTree;
 
     public float xSpread;
     public float ySpread;
@@ -19,6 +27,10 @@ public class LevelGeneration : MonoBehaviour
     {
         for (int i = 0; i < numberOfTrees; i++) PlaceItems(tree);
         for (int i = 0; i < numberOfcampfires; i++) PlaceItems(campfire);
+        for (int i = 0; i < numberOfBush; i++) PlaceItems(bush);
+        for (int i = 0; i < numberOfBigRock; i++) PlaceItems(bigRock);
+        for (int i = 0; i < numberOfSnowman; i++) PlaceItems(snowman);
+        for (int i = 0; i < numberOfChristmasTree; i++) PlaceItems(christmasTree);
     }
 
     void PlaceItems(GameObject o)
